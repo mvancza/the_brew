3dd767e1532704829981a8ab30ca2fafd621b544
michelle.vancza <michelle.vanca@slalom.com>
2019-10-30 21:59:29 -0400

    setup sass

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//dependencies
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+
+//scss
+var SCSS_SRC = './src/Assets/scss/**/*.scss';
+var SCSS_DEST = './src/Assets/css';
+
+//compile
+gulp.task('compile_scss', () => {
+    return gulp.src(SCSS_SRC)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(changed(SCSS_DEST))
+    .pipe(gulp.dest(SCSS_DEST))
+
+});
+
+//detect changes
+gulp.task('watch_scss', () => {
+    return gulp.watch(SCSS_SRC, gulp.series('compile_scss'));
+});
+
+gulp.task('default', gulp.series('watch_scss'));
